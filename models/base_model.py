d9d6c13997f3c589c0c8527b39f01c3670a96049
nickuchida <nicholas.y.uchida@gmail.com>
2020-02-14 22:12:41 +0000

    starting to add kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,13 +13,21 @@ class BaseModel:
     Class BaseModel
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Init of BaseModel, assigning each instance with unique id
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or "updated_at":
+                    value = datetime.strptime(self.created_at or self.updated_at,
+                                          '%m-%d-%Y')
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
