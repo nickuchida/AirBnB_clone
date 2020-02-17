724e01d9941d388faa69f6f6be97c964c2253695
christinepang1 <christinezpang@gmail.com>
2020-02-17 19:17:55 +0000

    Implemented create and show in the command interpreter

--- console.py
+++ console.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 '''console for airbnb'''
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
     '''class for command prompt module'''
+    HClasses = {"BaseModel": BaseModel}
     prompt = '(hbnb) '
 
     def empty(self):
@@ -14,6 +18,9 @@ class HBNBCommand(cmd.Cmd):
         '''default help displays command options'''
         print('help commands')
 
+    def emptyline(self):
+        pass
+
     def do_EOF(self, line):
         '''EOF to exit program'''
         return True
@@ -21,6 +28,36 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         '''quit to exit program'''
         return True
+
+    def do_create(self, line):
+        if line and line in HBNBCommand.HClasses.keys():
+            k = HBNBCommand.HClasses[line]()
+            k.save()
+            print(k.id)
+        elif not line:
+            print("** class name missing **")
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        tokens = line.split()
+        if len(tokens) == 0:
+            print("** class name missing **")
+        elif len(tokens) == 1:
+            if tokens[0] not in self.HClasses.keys():
+                print("** class doesn't exist **")
+            else:
+                print("** instance id missing **")
+        elif len(tokens) == 2:
+            print("Tokens is 2")
+            print('-----')
+            if tokens[0] in self.HClasses.keys():
+                string = "{}.{}".format(tokens[0], tokens[1])
+                if string in storage.all().keys():
+                    print(storage.all()[string])
+                else:
+                    print("** no instance found **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
