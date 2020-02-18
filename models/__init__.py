cc3e85a689f641b590f819a3740818375bc66a95
christinepang1 <christinezpang@gmail.com>
2020-02-18 05:05:33 +0000

    Inserted dictionary of classes

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+Classes = {'BaseModel': BaseModel,
+               'User': User,
+               'Place': Place,
+               'State': State,
+               'City': City,
+               'Amenity': Amenity,
+               'Review': Review}
 
 storage = FileStorage()
 storage.reload()
