15cdcf3669107c840519aa262d2680754157ee6a
nickuchida <nicholas.y.uchida@gmail.com>
2020-02-19 02:53:51 +0000

    fixing unittest syntax

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''unittest for amenity'''
+import unittest
+from models.amenity import Amenity
+
+class TestAmenity(unittest.TestCase):
+    '''tests amenity class'''
+
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''unittest for review'''
+
+import unittest
+from models.review import Review
+
+class TestReview(unittest.Testcase):
+    '''tests review class'''
+
+def test_empty(self):
+    empty = Reivew()
+    self.assertEqual(new.place_id, '')
+    self.assertEqual(new.user_id, '')
+    self.assertEqual(new.text, '')
+
+def test_review(self):
+    newreview = Review(place_id='Place',
+                       user_id='User',
+                       text='place text')
+    self.assertEqual(newreview.place_id, 'Place')
+    self.assertEqual(newreview.user_id, 'User')
+    self.assertEqual(newreview.text, 'place text')
